262e349e2fa49472bfa0dd7d63c13097b876cd08
utsugi_m <utsugi.mitsunori@gmail.com>
2023-01-24 14:39:07 +0900

    initi

---
+++ src/views/BindingView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="vue">
+    <input type="text" v-model="value" />
+    <p>state: {{ value }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  data() {
+    return {
+      value: "",
+    };
+  },
+});
+</script>
